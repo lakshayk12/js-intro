ff084744ec60bb7cdd9c28894cb4fc7657cf5cac
lakshayk12 <lakshaycom4@gmail.com>
2020-09-16 00:38:27 +0530

    Create 01Variables.js

---
+++ 02basics/01Variables.js
@@ -0,0 +1,10 @@
+var fullName = "Lakshay Kumar";
+//camel case vairables, uppercase starting functions
+console.log(fullName);
+
+// fullName = prompt("Enter Name:"); // To use this, we need to attach this to a browser.
+console.log("Full Name is", fullName);
+
+console.log(`Full Name is ${fullName}.`); //the better way.
+
+console.log(3 ** 3); //3^3
