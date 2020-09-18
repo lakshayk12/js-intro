7c5e8f641760c4bea5ea0c560680c40e70d7a445
lakshayk12 <lakshaycom4@gmail.com>
2020-09-18 14:44:27 +0530

    Update 01Variables.js
    - Arrow fn
    - every
    - fill
    - filter
    - slice
    - splice

--- 02basics/01Variables.js
+++ 02basics/01Variables.js
@@ -27,3 +27,42 @@ function fun() {
 
 // this, gives access to the global context, browser has Window, node console has {} empty scope.
 console.log(this);
+
+// Arrow function
+funa = () => {
+    console.log(this);
+}
+
+funa();
+
+// every, do operation on every element
+isEven = (n) => {
+    return (n & 1) === 0;
+}
+var result = [2, 4, 5].every(isEven);
+console.log(result);
+
+//every with callback
+var result = [2, 4, 2].every((n) => {
+    return (n & 1) === 0;
+});
+console.log(result);
+
+//fill
+var t = [2, 3, 4, 5];
+console.log(t.fill(0, 2, 3)); //output [ 2, 3, 0, 5 ] 
+
+//filter
+t = [0, 3, 0, 5];
+console.log(t.filter((n) => {
+    return n != 0;
+}));
+
+//slice
+t = [0, 3, 0, 5];
+console.log(t.slice(1, 3));
+
+//splice
+t = [1, 2, 3, 4, 5];
+spliced_t = t.splice(1, 2, -1);
+console.log("splice:", t, spliced_t); //output -> splice: [ 1, -1, 4, 5 ] [ 2, 3 ]
