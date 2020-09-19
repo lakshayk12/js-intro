c1ba7bad09bc1b829098a606a0d7c9384d6b75d1
lakshayk12 <lakshaycom4@gmail.com>
2020-09-19 21:06:39 +0530

    Update objects.js
    Injecting user-defined methods in User, by not changing its declaration using proto

--- 03intermediate/objects.js
+++ 03intermediate/objects.js
@@ -24,3 +24,12 @@ var User = function (name, age) {
 
 var lakshay = new User("Lakshay", 100); //kind of, User is a class
 console.log(lakshay);
+
+//Injecting user-defined methods in User, by not changing its declaration
+User.prototype.getName = function () {
+    console.log(this.name);
+}
+
+var lakshay2 = new User("Lakshay2", 102); //not its the responsibility of "new" keyword to find all those user-defined injections.
+lakshay2.getName();
+//we can even set our own constructor, getters or setters etc.
